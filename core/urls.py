15cfb7f3dfd07df9a4e9e7a2318c557f24f5e751
Pietra Almeida <costapietra@gmail.com>
2024-10-07 22:33:54 -0300

    Implementa funcionalidades de CRUD para o modelo Livro

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import LivroListCreate, LivroDetail
 
 urlpatterns = [
-    path('', views.livro_list_create, name='livros-list-create'),
-    path('<int:pk>/', views.livro_detail, name='livro-detail'),
+    path('', LivroListCreate.as_view(), name='livros-list-create'),
+    path('<int:pk>/', LivroDetail.as_view(), name='livro-detail'),
 ]
